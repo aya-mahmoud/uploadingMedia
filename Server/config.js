b3e1f62c38c7aa2a5d166013287fee14e8253df6
aya-mahmoud <aya.mahmoud.lotfy@gmail.com>
2024-04-03 02:33:46 +0200

    initial commit

---
+++ Server/config.js
@@ -0,0 +1,5 @@
+const PORT = "3001";
+const DB_CONNECTION_STRING = "mongodb+srv://ayamahmoudlotfy:test@cluster0.njtf6g0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+module.exports = { PORT, DB_CONNECTION_STRING };
